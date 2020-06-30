88a7a19d2e572538401a8e705d79523a76998bed
Rodrigo Nunes <rodrigonl10.rn@gmail.com>
2020-06-29 22:06:39 -0300

    Habilitando CORS na API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 // o routes aqui se torna um middleware
@@ -11,6 +12,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
