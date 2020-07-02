151fcd1d2097a69716d4ed7782567fcfcdbbf683
Rodrigo Nunes <rodrigonl10.rn@gmail.com>
2020-07-01 22:41:07 -0300

    Logout da aplicação

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 // coloco em volta dos lugares que quero que tenha acesso ao contexto
 // AuthContext.Provider
