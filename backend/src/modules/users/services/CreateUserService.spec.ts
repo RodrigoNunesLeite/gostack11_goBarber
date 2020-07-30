e1e437290ceafb86e76e6282fb06b23fa07b855c
Rodrigo Nunes <rodrigonl10.rn@gmail.com>
2020-07-30 20:59:40 -0300

    Testando criacao do usuario

---
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,44 @@
+import AppError from '@shared/errors/AppErrors';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+
+// describe definimos a categoria do teste
+// nesse caso estamos dizendoq que os testes são do
+// CreateUser
+describe('CreateUser', () => {
+  // it é a mesma coisa de test, mas significa isso ou isto
+  // Traduzindo...isso deve e a descricao
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    // Esperando que o objeto gere um id
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
