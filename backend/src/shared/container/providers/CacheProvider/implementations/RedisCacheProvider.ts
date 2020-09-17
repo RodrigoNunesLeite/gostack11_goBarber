5c4c01088affd94242eb9bb808a875bf49df3364
Rodrigo Nunes <rodrigonl10.rn@gmail.com>
2020-09-17 14:45:08 -0300

    invalidando cache

--- backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -26,4 +26,20 @@ export default class RedisCacheProvider implements ICacheProvider {
   }
 
   public async invalidate(key: string): Promise<void> { }
+
+  public async invalidatePrefix(prefix: string): Promise<void> {
+    const keys = await this.client.keys(`${prefix}:*`);
+
+    /**
+     * pipeline é um recurso do redis mais performático para
+     * realizar diversas operações simultaneas
+     */
+    const pipeline = this.client.pipeline();
+
+    keys.forEach(key => {
+      pipeline.del(key);
+    });
+
+    await pipeline.exec();
+  }
 }
